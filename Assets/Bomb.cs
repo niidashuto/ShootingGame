a2a8ae0aca32d4bd5c77369ad61e036392a2ab75
niida_shuto <k021g1267@g.neec.ac.jp>
2022-06-21 14:48:22 +0900

    ボム手前まで

---
+++ Assets/Bomb.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キーボードのBが押されたら
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            //タグが同じオブジェクトをすべて取得する
+            GameObject[] enemyBulletObjects =
+                GameObject.FindGameObjectsWithTag("EnemyBullet");
+
+            //上で取得した全てのオブジェクトを消滅させる
+            for()
+        }
+    }
+}
